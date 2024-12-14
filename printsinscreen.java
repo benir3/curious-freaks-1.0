e19d0b87b3ebd3a64bae80ec6efd8b1ee0ecd9ef
BENIL R <160488348+benir3@users.noreply.github.com>
2024-12-15 00:04:11 +0530

    Add files via upload

---
+++ printsinscreen.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class printsinscreen {
+  
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      String b = sc.next();
+      char c = sc.next().charAt(0);
+      System.out.println(a);
+      System.out.println(b);
+      System.out.println(c);
+   }
+}
