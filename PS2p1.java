3ea1193dafe4bfc49569f46b83483fa49742d1f1
BENIL R <160488348+benir3@users.noreply.github.com>
2024-12-22 11:10:03 +0530

    Add files via upload

---
+++ PS2p1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class PS2p1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the two values:");
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        for(int i =0;i<y;i++ )
+
+        {
+            System.out.println(x);
+
+
+        }
+
+
+        
+    }
+}
