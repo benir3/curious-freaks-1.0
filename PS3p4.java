49ce1e4138ea12ee02eb6742a3874b00a343eec6
BENIL R <160488348+benir3@users.noreply.github.com>
2024-12-30 12:19:25 +0530

    Add files via upload

---
+++ PS3p4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class PS3p4{
+    public static void main(String[] args){
+        int n=6;
+        for(int i=6;i>=1;i--){
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+
+        }
+        
+    }
+}
